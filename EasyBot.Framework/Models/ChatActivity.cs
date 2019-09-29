9593eb488997c1563254c06ca89a7ea540648ff4
Dmitriy Bashinskiy <sbmbash@gmail.com>
2019-09-29 14:01:50 +0300

    more models

--- EasyBot.Framework/Models/ChatActivity.cs
+++ EasyBot.Framework/Models/ChatActivity.cs
@@ -1,14 +1,19 @@
-﻿namespace EasyBot.Framework.Models
+﻿using System.IO;
+
+namespace EasyBot.Framework.Models
 {
     public abstract class ChatActivity
     {
+        public string Id { get; set; }
         public Chat Chat { get; set; }
+        public User Sender { get; set; }
 
         public TextActivity CreateActivity(string text)
         {
             return new TextActivity
             {
                 Chat = Chat,
+                Sender = null,
                 Text = text
             };
         }
@@ -17,5 +22,10 @@
     public class TextActivity : ChatActivity
     {
         public string Text { get; set; }
+    }
+
+    public class FileActivity : ChatActivity
+    {
+        public Stream FileStream { get; set; }
     }
 }
